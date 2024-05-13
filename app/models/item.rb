598d5a7629755efaf9c3fe412437c0fed6ff0513
yasuda23 <ysuda11412@gmail.com>
2024-05-13 21:04:40 +0900

    商品出品機能　指摘事項の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,11 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true, message: "can only save half-width numbers." }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   belongs_to :user
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :who_pay
+  belongs_to :delivery_area
+  belongs_to :delivery_day
+
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:item)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user: @user)
   end
 
   describe '商品出品機能' do
+
     context '必須項目が全て正しく入力されている場合' do
+      it '全ての項目が正しい値なら登録できること' do
+        expect(@item).to be_valid
+      end
+    end
+    
+  context  '商品登録ができない時' do
   
     it '商品画像を1枚つけることが必須であること' do
       @item.image = nil
       @item.valid?
       expect(@item.errors.full_messages).to include ("Image can't be blank")
     end
+
 
     it '商品名が必須であること' do
       @item.name = ''
@@ -79,7 +88,6 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is not a number')
     end
-    
   end
   end
 end
