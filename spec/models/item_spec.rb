2cc70c71c6d8151d2548c306fa15b463e1552349
yasuda23 <ysuda11412@gmail.com>
2024-05-13 21:18:44 +0900

    商品出品機能　指摘事項の修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品機能' do
 
-    context '必須項目が全て正しく入力されている場合' do
+    context '商品登録ができる時' do
       it '全ての項目が正しい値なら登録できること' do
         expect(@item).to be_valid
       end
     end
     
-  context  '商品登録ができない時' do
+    context  '商品登録ができない時' do
   
     it '商品画像を1枚つけることが必須であること' do
       @item.image = nil
@@ -88,6 +88,13 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is not a number')
     end
+
+    it 'userが紐づいていなければ登録できないこと' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
+    
   end
   end
 end
