82a041d7a53a6d933056d00ed0aaf8a8d0e5ee5f
yasuda23 <ysuda11412@gmail.com>
2024-05-15 14:51:56 +0900

    商品情報編集機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
 
-  validates :image, :name, :explanation, :price, presence: true
+  validates :image, :name, :explanation, :price, :category_id, :quality_id, :who_pay_id, :delivery_area_id, :delivery_day_id, presence: true
   validates :category_id, :quality_id, :who_pay_id, :delivery_area_id, :delivery_day_id, numericality: { other_than: 1 }
   validates :price, presence: true, numericality: { only_integer: true, message: "can only save half-width numbers." }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
